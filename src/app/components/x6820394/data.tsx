31b2f32211b33bd98793d368cd1678d3f949a0cf
nyovhz <120274887+nyovhz@users.noreply.github.com>
2025-11-20 22:11:33 -0600

    update playlist

--- src/app/components/x6820394/data.tsx
+++ src/app/components/x6820394/data.tsx
@@ -21,6 +21,11 @@ const device_playlist = [
     src:"https://res.cloudinary.com/dp39ooacq/video/upload/v1758841951/JOSUNE-AJUSCO_ezhc9g.mp3"
   },
   {
+    title: "Liars",
+    artist: "K I A",
+    src:"https://res.cloudinary.com/dp39ooacq/video/upload/v1763696999/full_of_liars_complete_si5pbg.wav"
+  },
+  {
     title: "SoloMusicaRomantica",
     artist: "Donatella Chiara",
     src:"https://res.cloudinary.com/dp39ooacq/video/upload/v1761288093/Solo_Musica_Romantica-Donatella_Chiara_khss3n.mp3"
@@ -34,6 +39,16 @@ const device_playlist = [
     title: "Lame Drums",
     artist: "Soft Wally",
     src: "https://res.cloudinary.com/dp39ooacq/video/upload/v1758189639/Lame_drums_-_SoftWally_oji6sv.mp3"
+  },
+  {
+    title: "idonteverwantyoutowearunderwarearoundmeagain",
+    artist: "d3br1s",
+    src: "https://res.cloudinary.com/dp39ooacq/video/upload/v1763697004/d3br1s_-_I_don_t_ever_want_you_to_wear_underware_around_me_again_vnwl43.wav"
+  },
+  {
+    title: "abionito",
+    artist: "0x1do",
+    src: "https://res.cloudinary.com/dp39ooacq/video/upload/v1763696973/0x1do_-_abionito_wyahyl.mp3"
   },
   {
     title: "Soledad",
